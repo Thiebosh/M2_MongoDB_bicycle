1849d85f8f98abda2f664b29dd5bfee95e8ee133
Ychixm <yochix.M@hotmail.fr>
2021-09-27 13:43:33 +0200

    4.5 wip $group working

--- exo4/exo4_5/exo.py
+++ exo4/exo4_5/exo.py
@@ -1,12 +1,41 @@
+def searchByStats(collection_live, collection_history, compare, ratio, begin_hour, begin_minutes, end_hour, end_minutes,
+                  begin_week, end_week):
 
-def searchByStats(collection_live, collection_history, compare, ratio, begin_hour, begin_minuts, end_hour, end_minuts, begin_week, end_week):
-    print(compare)
-    print(ratio)
-    print(begin_hour)
-    print(begin_minuts)
-    print(end_hour)
-    print(end_minuts)
-    print(begin_week)
-    print(end_week)
+    begin_week += 1
+    end_week += 1
+    aggregation = [
+        {
+            "$group":
+                {
+                    "_id": {"station": "$station_id",
+                            "dayOfWeek": {"$dayOfWeek": "$record_timestamp"},
+                            "hourOfDay": {"$hour": "$record_timestamp"}},
+                    "bike_available_avg": {"$avg": "$nbvelosdispo"},
+                    "stand_available_avg": {"$avg": "$nbplacesdispo"}
+                }
+
+        },
+        {
+            "$match": {
+                "dayOfWeek": {
+                    "$in": {
+                        list(range(begin_week, end_week + 1))
+                    }
+                }
+            }
+        },
+    ]
+    print("start")
+    print(list(collection_history.aggregate(aggregation)))
+    print("end")
+
+    # print(compare)
+    # print(ratio)
+    # print(begin_hour)
+    # print(begin_minutes)
+    # print(end_hour)
+    # print(end_minutes)
+    # print(begin_week)
+    # print(end_week)
 
     return []
